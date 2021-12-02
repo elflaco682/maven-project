914a78335cf91bb4d54a09be4577bd053e8e5a4c
elflaco682 <94693369+elflaco682@users.noreply.github.com>
2021-12-02 09:49:09 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,12 @@ public class Greeter {
   public Greeter() {
 
   } 
+   
   /**
    * @param someone name of person
    * @return greeting string
    */
   public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
+    return String.format("Hello, %s!", someone);
   }
 }
