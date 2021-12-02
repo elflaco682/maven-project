bdff6617eae7fea4edc72bce88912cb07805f6f7
elflaco682 <94693369+elflaco682@users.noreply.github.com>
2021-12-02 11:34:31 +0100

    Update package-info.java

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,10 +1,4 @@
 /**
- * Domain classes used to produce the greater file.
- * <p>
- * These classes contain the JAXB annotations.
- *
- * @since 1.0
- * @author elflaco
- * @version 1.1
+ * Javadoc du package.
  */
 package com.example;
